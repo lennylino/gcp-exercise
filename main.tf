f94b2cd3518a9726e848a50d4d499db6e36ae84b
lennylino <eleojolinox@gmail.com>
2024-10-17 11:13:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,16 +50,36 @@ resource "google_compute_subnetwork" "subnet" {
   region        = "us-central1"
     network     = "vpclino"
 }
-resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet1"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1"
-  network       = "vpclino"
-}
 
  resource "google_compute_subnetwork" "subnet-02" {
    name          = "subnet2"
    ip_cidr_range = "10.0.2.0/24"
    region        = "europe-west10"
    network       = "vpclino"
+}
+
+provider "google" {
+  project = "project-lino-433318"
+  region = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+    name = "bucket-lino"
+    location = "us"
+    storage_class = "standard"
+}
+
+resource "google_storage_bucket" "my_bucket-02" {
+    name = "regional_bucket"
+    location = "us"
+    storage_class = "regional"
+}
+
+resource "google_sql_database_instance" "my_mysql_instance" {  
+  name         = "lino-database"
+  region       = "us-central1" 
+  database_version = "MYSQL"
+    tier = "db-n1-standard-1"
+    disk_size_gb = 10 
+  }
 }
